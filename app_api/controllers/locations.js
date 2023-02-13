e5a410a9e872cabfa8f111d908a3307300aceb73
akkoshta786 <atulk2018@gmail.com>
2023-02-14 02:20:22 +0530

    write locationsListByDistance method - use $geoNear to list locations based on geographical distance - remove coords field from locationSchema - add locations field in locationSchema - change seed data to follow above changes

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,44 +1,93 @@
-const mongoose = require('mongoose');
-const Loc = mongoose.model('Location');
+const mongoose = require("mongoose");
+const Loc = mongoose.model("Location");
 
-module.exports.locationsListByDistance = (req, res) => {
-  sendJsonResponse(res, 200, { 'status': 'success' })
-}
+const earthRadius = 6371;
 
-module.exports.locationsCreate = (req, res) => {
+module.exports.locationsListByDistance = async (req, res) => {
+  const lng = parseFloat(req.query.lng);
+  const lat = parseFloat(req.query.lat);
+  const maxDist = parseFloat(req.query.maxdist) || 1; // in km
+  const limit = parseInt(req.query.limit) || 2;
 
-}
+  if (!lat || !lng) {
+    sendJsonResponse(res, 404, {
+      message: "lng and lat query parameters are required",
+    });
+    return;
+  }
+
+  try {
+    let locations = [];
+    const geoOptions = {
+      let: { pt: [lng, lat] },
+    };
+
+    const fetchedLocations = await Loc.aggregate(
+      [
+        {
+          $geoNear: {
+            near: "$$pt",
+            distanceField: "distance",
+            distanceMultiplier: earthRadius,
+            maxDistance: maxDist / earthRadius,
+            includeLocs: "dist.location",
+            spherical: true,
+          },
+        },
+      ],
+      geoOptions
+    ).limit(limit);
+
+    if (fetchedLocations.length == 0) {
+      sendJsonResponse(res, 200, { message: "No result found" });
+      return;
+    }
+
+    fetchedLocations.forEach((loc) => {
+      locations.push({
+        distance: loc.distance,
+        name: loc.name,
+        address: loc.address,
+        rating: loc.rating,
+        facilities: loc.facilities,
+        _id: loc._id,
+      });
+    });
+
+    sendJsonResponse(res, 200, locations);
+  } catch (e) {
+    sendJsonResponse(res, 500, {
+      message: "An error has occured while fetching locations",
+    });
+  }
+};
+
+module.exports.locationsCreate = (req, res) => {};
 
 module.exports.locationsReadOne = (req, res) => {
   if (req.params && req.params.locationid) {
-    Loc.findById(req.params.locationid)
-      .exec((err, location) => {
-        if (!location) {
-          sendJsonResponse(res, 404, {
-            'message': 'locationid not found'
-          });
-          return;
-        } else if (err) {
-          sendJsonResponse(res, 404, err);
-          return;
-        }
-        sendJsonResponse(res, 200, location);
-      })
+    Loc.findById(req.params.locationid).exec((err, location) => {
+      if (!location) {
+        sendJsonResponse(res, 404, {
+          message: "locationid not found",
+        });
+        return;
+      } else if (err) {
+        sendJsonResponse(res, 404, err);
+        return;
+      }
+      sendJsonResponse(res, 200, location);
+    });
   } else {
     sendJsonResponse(res, 404, {
-      'message': 'No locationid in request'
+      message: "No locationid in request",
     });
   }
+};
 
-}
+module.exports.locationsUpdateOne = (req, res) => {};
 
-module.exports.locationsUpdateOne = (req, res) => {
-
-}
-
-module.exports.locationsDeleteOne = (req, res) => {
-
-}
+module.exports.locationsDeleteOne = (req, res) => {};
 
 const sendJsonResponse = function (res, status, content) {
   res.status(status);
--- app_api/models/location.js
+++ app_api/models/location.js
@@ -1,28 +1,31 @@
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
 var openingTimeSchema = new mongoose.Schema({
   days: { type: String, required: true },
   opening: String,
   closing: String,
-  closed: { type: Boolean, required: true }
-})
+  closed: { type: Boolean, required: true },
+});
 
 var reviewSchema = new mongoose.Schema({
   _id: String,
   author: String,
   rating: { type: Number, required: true, min: 0, max: 5 },
   reviewText: String,
-  createdOn: { type: Date, "default": Date.now }
-})
+  createdOn: { type: Date, default: Date.now },
+});
 
 var locationSchema = new mongoose.Schema({
   name: { type: String, required: true },
   address: String,
-  rating: { type: Number, "default": 0, min: 0, max: 5 },
+  rating: { type: Number, default: 0, min: 0, max: 5 },
   facilities: [String],
-  coords: { type: Number, index: '2dsphere' },
+  location: {
+    type: { type: String, default: "Point" },
+    coordinates: [Number],
+  },
   openingTimes: [openingTimeSchema],
-  reviews: [reviewSchema]
-});
+  reviews: [reviewSchema],
+}).index({ location: "2dsphere" });
 
-mongoose.model('Location', locationSchema);
+mongoose.model("Location", locationSchema);
